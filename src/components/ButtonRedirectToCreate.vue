1e96a9ce7eb36f468733cc52d61066750f855f2b
davipferr <davipferr@gmail.com>
2023-05-22 16:01:59 -0300

    feat(): Cria os botões de redirecionamento
    para as páginas de criação e lista de tarefas

---
+++ src/components/ButtonRedirectToCreate.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="justify-content-start d-flex">
+    <v-btn
+      style="background-color: #2938D5; color: white;"
+      @click="redirectToCreate"
+      :disabled="loading"
+      title="Ir para págian de Criação de Tarefas"
+    >
+      Criar Tarefa 
+      <v-icon>
+        mdi-plus
+      </v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    redirectToCreate() {
+      this.$router.push('/create');
+    },
+  }
+};
+</script>
---
+++ src/components/ButtonRedirectToList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="justify-content-end d-flex">
+    <v-btn
+      style="background-color: #45BE14; color: black;"
+      @click="redirectToList"
+      :disabled="loading"
+      title="Ir para a página de Listar Tarefas"
+    >
+      Lista de Tarefas 
+      <v-icon class="ml-3 justify-content-center">
+        mdi-format-list-bulleted
+      </v-icon>
+    </v-btn>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    redirectToList() {
+      this.$router.push('/list');
+    },
+  }
+};
+</script>
